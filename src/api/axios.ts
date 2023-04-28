a46c1c3897fa7c3750a676b6bd86b8d323f76cd5
Jiri Trefil <badjokke@gmail.com>
2023-04-28 15:16:28 +0200

    #10527: axios vytvari authorized/unauthorized requesty v zavilosti na tom, zda ma uzivatel token nebo ne

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, {AxiosInstance} from 'axios';
 import {retrieveBearerCookie, retrieveJwtToken} from "../context/LocalStorageManager";
 const BASE_URL = 'http://localhost:8080/v2';
 
@@ -8,18 +8,35 @@ export default axios.create({
    baseURL: BASE_URL
 });
 
+
+// if the user doesnt have token, we will create request w/o authorization header
+const createUnauthorizedRequest = () => {
+   const axiosObject:AxiosInstance = axios.create({
+      baseURL: BASE_URL,
+      headers: {'Content-Type': 'application/json'},
+      withCredentials: true
+   });
+   return axiosObject;
+}
+
+
+//entry point for creating axios instances
+//this 'function' SHOULD always be used when creating HTTP request
 export const axiosPrivate = () => {
    //const token = retrieveJwtToken();
    const token = retrieveBearerCookie();
+   //I dont have a token => my request will be created as unauthorized
    if(token == null) {
-      //non existent token
-      //user must first log in
-      return null;
+      return createUnauthorizedRequest();
    }
-   return axios.create({
+   //I have a token => I can have Authorization header in my request with bearer token
+   const axiosObject:AxiosInstance = axios.create({
       baseURL: BASE_URL,
-      headers: {'Content-Type': 'application/json', Authorization: `Bearer ${token}`},
+      headers: {'Content-Type': 'application/json',Authorization: `Bearer ${token}`},
       withCredentials: true
    });
+
+
+   return axiosObject;
 }
 
