fe4e307e56b45a7eae3fed528bb037c464b8e4b5
Hrabikv <hrabikv@students.zcu.cz>
2023-10-15 21:46:05 +0200

    https://kivprogrammers.atlassian.net/browse/TSP2-32 úvodní práce na strácnce konfigurace

--- src/api/APIConfiguration.ts
+++ src/api/APIConfiguration.ts
@@ -3,11 +3,13 @@ import ApiCaller, {API_RESPONSE, HTTP_METHOD} from "../components/api/ApiCaller"
 const CONFIGURATION_URL = '/configuration/configuration';
 const CONFIGURATION_NAME_URL = '/configuration/configuration_name';
 
-export const fetchConfigurations = async (username:string): Promise<API_RESPONSE> => {
-    const data:{} = JSON.stringify({name:username});
+export const fetchOneConfiguration = async (username:string, configurationId:string): Promise<API_RESPONSE> => {
+    const requestBody:{} = {user:{name:username},id:configurationId};
+    const data:{} = JSON.stringify(requestBody);
     const response: API_RESPONSE = await ApiCaller(data, CONFIGURATION_URL, HTTP_METHOD.POST);
     return response;
 }
+
 
 export const fetchConfigurationNames = async (username:string): Promise<API_RESPONSE> => {
     const data:{} = JSON.stringify({name:username});
